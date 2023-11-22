7a94c743e83789e1583a93c85f6ea37d04a92099
VladyslavHn <vlad.gnatok@gmail.com>
2023-11-22 19:32:23 +0100

    markup-hw03

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,16 @@
-function getElementWidth(content, padding, border) {
-     return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+function filterArray(numbers, value) {
+     const array = [];
+     for (const num of numbers) {
+          if (num > value) {
+               array.push(num);
+          }
+     }
+     return array;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20));
